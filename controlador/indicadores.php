7c394e62d8f4bf12c294aa7a8acfb13f4333aa07
Unknown <d_recinos@outlook.com>
2019-01-23 22:37:31 -0600

    fsdfdsf

--- controlador/indicadores.php
+++ controlador/indicadores.php
@@ -6,145 +6,69 @@
 <?php
 $nr = $_POST['nr'];
 	$conexion = mysqli_connect("162.241.252.245","proyedk4_WPZF0","MAYO_nesa94","proyedk4_WPZF0"); 
-	$query_consulta_revision=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_oficial WHERE nr_empleado = '".$nr."' AND estado IN(1,3,5)"); 
-	while( $filar=mysqli_fetch_array($query_consulta_revision)){
-			$indicador_revision[] = $filar;
+	$query_consulta_cumple=mysqli_query($conexion,"select UPPER(CONCAT_WS(' ', e.primer_nombre, e.segundo_nombre, e.tercer_nombre, e.primer_apellido, e.segundo_apellido, e.apellido_casada)) AS nombre_empleado,
+			e.fecha_nacimiento, TIMESTAMPDIFF(YEAR,e.fecha_nacimiento,CURDATE()) AS edad from sir_empleado e where id_estado= 1 and DATE_FORMAT(e.fecha_nacimiento, '%m') = '".date("m")."' order by DATE_FORMAT(e.fecha_nacimiento, '%d')"); 
+	$letras=mysqli_num_rows($query_consulta_cumple);
+	while( $filacumple=mysqli_fetch_array($query_consulta_cumple)){
+			$indicador_cumple[] = $filacumple;
 			}
-	foreach ($indicador_revision as $fila_indicador_revision) {}
+	foreach ($indicador_cumple as $fila_indicador_cumple) {}
 
-	$query_consulta_observacion=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_oficial WHERE nr_empleado = '".$nr."' AND estado IN(2,4,6)"); 
-	while( $filao=mysqli_fetch_array($query_consulta_observacion)){
-			$indicador_observacion[] = $filao;
-			}
-	foreach ($indicador_observacion as $fila_indicador_observacion) {}
+	$mesesarray = array('ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE');
 
-	$query_consulta_pagada=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_oficial WHERE nr_empleado = '".$nr."' AND estado = 8"); 
-	while( $filap=mysqli_fetch_array($query_consulta_pagada)){
-			$indicador_pagada[] = $filap;
-			}
-	foreach ($indicador_pagada as $fila_pagada) {}
 
-	$query_pasaje_revision=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_pasajes WHERE nr = '".$nr."' AND estado IN(1,3,5)"); 
-	while( $filapr=mysqli_fetch_array($query_pasaje_revision)){
-			$indicador_pasaje_revision[] = $filapr;
+	$query_consulta_empleados=mysqli_query($conexion,"select SUM(CASE WHEN id_genero = '1' THEN 1 ELSE 0 END) masc,
+							SUM(CASE WHEN id_genero = '2' THEN 1 ELSE 0 END) feme,
+							COUNT(id_empleado) AS total from sir_empleado where id_estado=1"); 
+	while( $filaemple=mysqli_fetch_array($query_consulta_empleados)){
+			$indicador_emple[] = $filaemple;
 			}
-	foreach ($indicador_pasaje_revision as $fila_pasaje_revision) {}
+	foreach ($indicador_emple as $fila_indicador_emple) {}
 
-	$query_pasaje_observados=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_pasajes WHERE nr = '".$nr."' AND estado IN(2,4,6)"); 
-	while( $filapo=mysqli_fetch_array($query_pasaje_observados)){
-			$indicador_pasaje_observado[] = $filapo;
+	$query_consulta_docs=mysqli_query($conexion,"select UPPER(CONCAT_WS(' ', e.primer_nombre, e.segundo_nombre, e.tercer_nombre, e.primer_apellido, e.segundo_apellido, e.apellido_casada)) AS nombre_empleado,
+			e.fecha_vencimiento_dui, e.fecha_vencimiento_licencia_armas, 
+			(CASE WHEN fecha_vencimiento_dui = '0000-00-00' THEN '1' WHEN fecha_vencimiento_dui IS NULL THEN '1' WHEN e.fecha_vencimiento_dui <= '".date("Y-m-d")."' THEN '0' ELSE '2' END) dias_dui, 
+			(CASE WHEN fecha_vencimiento_licencia_armas = '0000-00-00' THEN '1' WHEN fecha_vencimiento_licencia_armas IS NULL THEN '1' WHEN e.fecha_vencimiento_licencia_armas <= '".date("Y-m-d")."' THEN '0' ELSE '2' END) dias_licencia_arma from sir_empleado e where id_estado=1 and ((e.fecha_vencimiento_dui <> '0000-00-00' AND e.fecha_vencimiento_dui <= '".date("Y-m-d")."') OR (e.fecha_vencimiento_licencia_armas <> '0000-00-00' AND e.fecha_vencimiento_licencia_armas <= '".date("Y-m-d")."'))"); 
+	$numdoc=mysqli_num_rows($query_consulta_docs);
+	while( $filadocs=mysqli_fetch_array($query_consulta_docs)){
+			$indicador_docs[] = $filadocs;
 			}
-	foreach ($indicador_pasaje_observado as $fila_pasaje_observado) {}
-
-	$query_pasaje_pagado=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_pasajes WHERE nr = '".$nr."' AND estado = 8"); 
-	while( $filapp=mysqli_fetch_array($query_pasaje_pagado)){
-			$indicador_pasaje_pagado[] = $filapp;
-			}
-	foreach ($indicador_pasaje_pagado as $fila_pasaje_pagado) {}
-
-	$query_permiso_autorizar=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_informacion_empleado WHERE nr_jefe_inmediato = '".$nr."' OR nr_jefe_departamento = '".$nr."'"); 
-	while( $filapermiso=mysqli_fetch_array($query_permiso_autorizar)){
-			$indicador_permiso[] = $filapermiso;
-			}
-	foreach ($indicador_permiso as $fila_permisos_autorizar) {}
-
-	$query_pasaje_autorizar=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_pasajes WHERE (nr_jefe_inmediato = '".$nr."' AND estado = 1) OR (nr_jefe_regional = '".$nr."' AND estado = 3)"); 
-	while( $filapa=mysqli_fetch_array($query_pasaje_autorizar)){
-			$indicador_pa[] = $filapa;
-			}
-	foreach ($indicador_pa as $fila_indicador_pa) {}
-
-	$query_viatico_autorizar=mysqli_query($conexion,"SELECT count(*) AS cantidad FROM vyp_mision_oficial WHERE (nr_jefe_inmediato = '".$nr."' AND estado = 1) OR (nr_jefe_regional = '".$nr."' AND estado = 3)"); 
-	while( $filapv=mysqli_fetch_array($query_viatico_autorizar)){
-			$indicador_pv[] = $filapv;
-			}
-	foreach ($indicador_pv as $fila_indicador_pv) {}
+	foreach ($indicador_docs as $fila_indicador_docs) {}
 ?>
-<div class="container-fluid">
-	<div class="row page-titles">
-           <div class="align-self-center" align="center">
-               <h3 class="text-themecolor m-b-0 m-t-0">Indicadores de solicitud de viáticos</h3>
-           </div>
-       	</div>
+<div class="container-fluid"> <br>
 	<div class="row">       	
-		<?php if($fila_permisos_autorizar>0){ ?>
-	        <!-- Column -->
-	        <div class="col-md-6 col-lg-3 col-xlg-3" >
-	            <div class="card card-inverse card-dark">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_indicador_pv[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes para autorizar</h5>
-	                </div>
-	            </div>
-	        </div>
-	        <!-- Column -->
-	    	<?php } ?>
-		<div class="col-md-6 col-lg-3 col-xlg-3">
-		    <div class="card card-inverse card-info" >
-		        <div class="box bg-info text-center">
-		            <h1 class="font-light text-white"><?php echo $fila_indicador_revision[0];?></h1>
-		            <h5 class="text-white">Solicitudes en revisión</h5>
-		        </div>
-		    </div>
-		</div>
-		<div class="col-md-6 col-lg-3 col-xlg-3">
-	            <div class="card card-primary card-inverse">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_indicador_observacion[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes observadas</h5>
-	                </div>
-	            </div>
-	    </div>
-	    <div class="col-md-6 col-lg-3 col-xlg-3" >
-	            <div class="card card-inverse card-success">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_pagada[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes pagadas</h5>
-	                </div>
-	            </div>
-	     </div>
+		<div class="col-lg-4">
+                <div class="card card-inverse card-primary">
+                    <div class="card-body" style="cursor: pointer;" onclick="" align="center">
+                        <h1 class="text-white"><i class="mdi mdi-cake"></i> &emsp;<small><?php echo $letras; if($letras==1) { echo " Persona"; }else{ echo " Personas"; } ?></small></h1>
+                        <h3 class="card-title"><?php if($letras==1) { echo "Cumple "; }else{ echo "Cumplen "; } ?>años en <?=$mesesarray[date("m")-1]?><br></h3>
+                    </div>
+                </div>
+            </div>
+        <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 align="center">Personas empleadas</h4>
+                        <div class="d-flex flex-row" align="center">
+                            <div class="p-0 b-r" align="center" style="width: 50%;">
+                                <h6 class="font-light">Hombres</h6><h6><?php echo $fila_indicador_emple[0]; ?></h6><h6><?php echo number_format($fila_indicador_emple[0]/$fila_indicador_emple[2]*100,2); ?>%</h6>
+                            </div>
+                            <div class="p-0" align="center" style="width: 50%;">
+                                <h6 class="font-light">Mujeres</h6><h6><?php echo $fila_indicador_emple[1]; ?></h6><h6><?php echo number_format($fila_indicador_emple[1]/$fila_indicador_emple[2]*100,2); ?>%</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="col-lg-4">
+                <div class="card card-inverse card-success">
+                    <div class="card-body" style="cursor: pointer;" onclick="" align="center">
+                        <h1 class="text-white"><i class="mdi mdi-document"></i> &emsp;<small><?php echo $numdoc; if($numdoc==1) { echo " Persona"; }else{ echo " Personas"; } ?></small></h1>
+                        <h3 class="card-title"><?php if($numdoc==1) { echo "Tiene "; }else{ echo "Tienen "; } ?>documentos vencidos<br></h3>
+                    </div>
+                </div>
+            </div>
 	</div>
-	<div class="row page-titles">
-           <div class="align-self-center" align="center">
-               <h3 class="text-themecolor m-b-0 m-t-0">Indicadores de solicitud de pasajes</h3>
-           </div>
-    </div>
-    <div class="row">
-    	<?php if($fila_permisos_autorizar>0){?>
-    	<div class="col-md-6 col-lg-3 col-xlg-3" >
-	            <div class="card card-inverse card-dark">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_indicador_pa[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes para autorizar</h5>
-	                </div>
-	            </div>
-	        </div>
-	      <?php }?>
-    	 <div class="col-md-6 col-lg-3 col-xlg-3">
-	            <div class="card card-inverse card-info">
-	                <div class="box bg-info text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_pasaje_revision[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes en revisión</h5>
-	                </div>
-	            </div>
-	    </div>
-		<div class="col-md-6 col-lg-3 col-xlg-3"  >
-	            <div class="card card-primary card-inverse">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_pasaje_observado[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes observadas</h5>
-	                </div>
-	            </div>
-	    </div>
-	    <div class="col-md-6 col-lg-3 col-xlg-3" >
-	            <div class="card card-inverse card-success">
-	                <div class="box text-center">
-	                    <h1 class="font-light text-white"><?php echo $fila_pasaje_pagado[0]; ?></h1>
-	                    <h5 class="text-white">Solicitudes pagadas</h5>
-	                </div>
-	            </div>
-	        </div>
-    </div>
 </div>
 </body>
 </html>
---
+++ controlador/info_personal.php
@@ -0,0 +1,88 @@
+<?php
+$funcion = $_POST['funcion'];
+$id_empleado = $_POST['nr']; 
+switch($funcion) {
+        case 'reporte': 
+            echo reporte_info_empleado($id_empleado);
+            break;
+    }
+	
+
+function reporte_info_empleado($id_empleado){
+
+      $conexion = mysqli_connect("162.241.252.245","proyedk4_WPZF0","MAYO_nesa94","proyedk4_WPZF0"); 
+      
+     $cabecera_vista = '<table style="font-size: 14;" class="table"><tr> 
+		<td width="950px"><h6><center>MINISTERIO DE TRABAJO Y PREVISION SOCIAL <br> DEPARTAMENTO DE RECURSOS HUMANOS <br>  INFORMACIÓN PERSONAL</center><h6></td>
+	 	</tr></table>';
+	 	$fecha=strftime( "%d-%m-%Y - %H:%M:%S", time() );
+	 	//$pie = 'Usuario: '.$this->session->userdata('usuario_viatico').'    Fecha y Hora Creacion: '.$fecha.'||{PAGENO} de {nbpg} páginas';
+
+	 	//$data = array('nr'=>'2588');
+		//$empleado_NR_viatico = $this->Reportes_viaticos_model->obtenerNREmpleadoViatico($data);
+		$query_consulta_nr=mysqli_query($conexion,"select * from org_usuario where nr='".$id_empleado."'");
+      	while( $fila_nombre=mysqli_fetch_array($query_consulta_nr)){
+            $empleado_nombre[] = $fila_nombre;
+         }
+		foreach ($empleado_nombre as $key_nombre) {	}
+		
+		//$ids = array('nr' => '2588');
+		//$viatico = $this->Reportes_viaticos_model->obtenerListaviatico_pendiente($ids);
+		$query_consulta=mysqli_query($conexion,"SELECT * FROM sir_empleado WHERE nr='".$id_empleado."'");
+		while( $fila=mysqli_fetch_array($query_consulta)){
+            $info[] = $fila;
+         }
+          
+         if($info){ 
+				foreach ($info as $informacion) {
+		$cuerpo = '
+		<h6 style="font-size: 12;">&nbsp;&nbsp;Empleado: '.($key_nombre[1]).'</h6>
+		<div class="table-responsive">
+			<table  class="table" style="font-size: 14;">
+				<thead >
+					<tr>
+						<th >Primer Nombre</th>
+						<th >Segundo Nombre</th>
+						<th >Primer Apellido</th>
+						<th >Segundo Apellido</th>
+						<th >Pasajes</th>
+						<th >Alojamiento</th>
+						<th  >Total</th>						 
+					</tr>
+					<tr>
+						
+					</tr>
+				</thead>
+				<tbody>
+					
+					';
+
+					$cuerpo .= '
+						<tr>
+							<td>'.$informacion[1].'</td>
+							<td>'.$informacion[2].'</td>
+							<td>'.$informacion[3].'</td>
+							<td>'.$informacion[4].'</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						';
+					
+					}
+				}else{
+				$cuerpo .= '
+						<tr><td colspan="10"><center>No hay registros "'.$id_empleado.'"</center></td></tr>
+					';
+				}
+				$cuerpo .= '
+				</tbody>
+			</table>
+			</div>
+        ';  
+
+         mysqli_close($conexion);
+      echo $cabecera_vista.=$cuerpo;
+     
+}
+?>
